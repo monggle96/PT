23dccae853e83e838b6ff4fb47ac94a005cda98d
monggle96 <seonulog@gmail.com>
2026-09-17 10:37:38 +0900

    Add files via upload

--- calculator/sw.js
+++ calculator/sw.js
@@ -1,6 +1,6 @@
 // PT 급여계산기 오프라인 캐시
 // 앱을 수정한 뒤에는 아래 VERSION 값을 바꿔야 기기에 새 버전이 반영됩니다.
-const VERSION = 'ptpay-v13';
+const VERSION = 'ptpay-v14';
 const FILES = ['./', './index.html', './manifest.webmanifest', './icon.png'];
 
 self.addEventListener('install', e => {
